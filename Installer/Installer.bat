0480d1e4e1f2097ecff20855528f8307fe73d801
tecnologer <rdominguez@tecnologer.net>
2019-03-18 15:41:08 -0700

    Installer.bat
    Automation for build projects, Font Installer and MSI creator

---
+++ Installer/Installer.bat
@@ -0,0 +1,45 @@
+cls
+@IF "%1" == "" GOTO ErrorVersion
+:: Environment Setup
+:: For 2017
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+:: Setting Variables..
+
+@SET version=%1
+@SET output=Output\
+@SET input=..\..\FontInstaller\FontInstaller\bin\Release\
+@IF "%PROCESSOR_ARCHITECTURE%"=="x86" (set CPUbit=x86) else (set CPUbit=x64)
+
+:: msbuild /verbosity:m /p:Configuration=Release ..\FontInstaller\InstallerAutomation\InstallerAutomation.sln
+
+@IF EXIST %output% GOTO NOTMAKE
+@mkdir %output%
+
+:NOTMAKE
+@del /Q /S %input%
+@del /Q /S %output%
+
+@echo Building solution
+msbuild /verbosity:m /p:Configuration=Release ..\..\FontInstaller\FontInstaller.sln
+if ERRORLEVEL 1 exit /B
+
+@echo Copy files to the output directory.
+@copy "%input%DotNetZip.dll" 				%output%
+@copy "%input%log4net.dll" 					%output%
+@copy "%input%Syroot.KnownFolders.dll" 		%output%
+@copy "%input%log4net.config" 				%output%
+@copy "%input%FontInstaller.exe"       		%output%
+
+:: HACK .. 3 second delay
+ping 127.0.0.1 -n 4 > nul
+
+msbuild /verbosity:m /p:Configuration=Release "..\AppInstaller\AppInstaller.sln"
+if ERRORLEVEL 1 exit /B
+
+@copy "..\AppInstaller\bin\Release\AppInstaller.msi" "%output%FontInstaller_%version%.msi"	
+
+
+@GOTO End
+:ErrorVersion
+@echo Missing parameter. Usage is "Installer.bat" "0.0.1.001" 
+:End
